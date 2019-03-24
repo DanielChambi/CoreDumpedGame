6e383a623592551825b75bd7e7c03368b057d3f4
Daniel Chambi Campos <leian@Daniels-MacBook-Pro.local>
2019-03-24 20:35:40 +0100

    Score system that add points automatically

---
+++ CoreDumpedGame/Assets/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;        // The player's scor
+
+    public int counterInit;
+    int counter;
+
+    public Text text;                      // Reference to the Text component.
+
+
+    void Awake()
+    {
+        // Set up the reference.
+
+        // Reset the score.
+        score = 0;
+
+        counter = counterInit;
+    }
+
+
+    void Update()
+    {
+        if (!PauseMenu.GameIsPaused)
+        {
+            counter--;
+        }
+        if (counter == 0)
+        {
+            score++;
+            counter = counterInit;
+        }
+        // Set the displayed text to be the word "Score" followed by the score value.
+        text.text = "Score: " + score;
+    }
+}
