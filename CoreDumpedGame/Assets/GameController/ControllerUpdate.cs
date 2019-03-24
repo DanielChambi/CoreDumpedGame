dee59d203369e457c67730966ec06aa670b22286
Daniel Chambi Campos <leian@Daniels-MacBook-Pro.local>
2019-03-24 20:12:50 +0100

    added Pause menu

---
+++ CoreDumpedGame/Assets/GameController/ControllerUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerUpdate : MonoBehaviour
+{
+    public GameObject Player = GameObject.FindWithTag("Player");
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            GlobalVarsInit.pause = !GlobalVarsInit.pause;
+            Player.GetComponent<Player>().enabled = !Player.GetComponent<Player>().enabled;           
+        }
+    }
+}
